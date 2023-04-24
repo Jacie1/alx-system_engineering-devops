836c98cf97d5917925f316317fb164029ec49e3b
Jacie1 <irinemwanga5@gmail.com>
2023-04-24 10:42:23 -0700

    task1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
+""" REST API for todo lists of employees"""
+
 import requests
 import sys
 
-EMPLOYEE_API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users/{}'
-TODO_API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos?userId={}'
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python todo_progress.py [employee_id]')
-        sys.exit(1)
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    employee_id = int(sys.argv[1])
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    # Fetch employee name
-    employee_response = requests.get(EMPLOYEE_API_ENDPOINT.format(employee_id))
-    employee_response.raise_for_status()
-    employee = employee_response.json()
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    # Fetch employee's TODO list
-    todo_response = requests.get(TODO_API_ENDPOINT.format(employee_id))
-    todo_response.raise_for_status()
-    todo_list = todo_response.json()
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    # Calculate TODO list progress
-    total_tasks = len(todo_list)
-    done_tasks = sum(1 for task in todo_list if task['completed'])
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-    # Print progress report
-    print('Employee {} is done with tasks({}/{})'.format(employee['name'], done_tasks, total_tasks))
-    for task in todo_list:
-        if task['completed']:
-            print('\t{} {}'.format('\t', task['title']))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
